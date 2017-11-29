2d33512999ec818f8cf881b7ea84056a1617ad50
Daria-Kurgaeva <Daria.Kurgaeva@veeam.com>
2017-11-29 19:07:22 +0300

    edits

--- js/pictures.js
+++ js/pictures.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var commentsArr = [
+var DEFAULT_COMMENTS = [
   'Всё отлично!',
   'В целом всё неплохо. Но не всё.',
   'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
@@ -8,6 +8,8 @@ var commentsArr = [
   'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
   'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'
 ];
+
+var PHOTOS_COUNT = 25;
 
 var pictureTemplate = document.querySelector('#picture-template').content;
 var picturesContainer = document.querySelector('.pictures');
@@ -17,45 +19,56 @@ var getRandomNumber = function (min, max) {
   return Math.floor((Math.random() * (max - min) + min));
 };
 
-var getRandomString = function (arr) {
+var getRandomArrElement = function (arr) {
   return arr[getRandomNumber(0, arr.length)];
+};
+
+var generateComments = function () {
+  var count = getRandomNumber(0, DEFAULT_COMMENTS.length);
+  var comments = [];
+  for (var i = 0; i < count; i++) {
+    var newCommentString = getRandomArrElement(DEFAULT_COMMENTS);
+    comments.push(newCommentString);
+  }
+  return comments;
 };
 
 var createPhotoBlock = function (photo) {
   var photoBlock = pictureTemplate.cloneNode(true);
   photoBlock.querySelector('img').src = photo.url;
   photoBlock.querySelector('.picture-likes').textContent = photo.likes;
-  photoBlock.querySelector('.picture-comments').textContent = photo.comments;
+  photoBlock.querySelector('.picture-comments').textContent = photo.comments.length;
   return photoBlock;
 };
 
-var createPhotoObject = function (numberOfObjects) {
-  var photo = {};
-  for (var i = 1; i <= numberOfObjects; i++) {
-    photo[i] = {
-      url: 'photos/' + i + '.jpg',
+var createPhotoArr = function (numberOfObjects) {
+  var photos = [];
+  for (var i = 0; i < numberOfObjects; i++) {
+    var photo = {
+      url: 'photos/' + (i + 1) + '.jpg',
       likes: getRandomNumber(15, 200),
-      comments: getRandomString(commentsArr)
+      comments: generateComments()
     };
+    photos.push(photo);
   }
-  return photo;
+  return photos;
 };
 
-var photoObject = createPhotoObject(25);
-
-var renderPhotoBlocks = function (numberOfObjects) {
+var renderPhotoBlocks = function (photos) {
   var fragment = document.createDocumentFragment();
-  for (var i = 1; i <= numberOfObjects; i++) {
-    fragment.appendChild(createPhotoBlock(photoObject[i]));
+  for (var i = 0; i < photos.length; i++) {
+    fragment.appendChild(createPhotoBlock(photos[i]));
   }
   return fragment;
 };
 
 var showPhotoBlocks = function () {
-  picturesContainer.appendChild(renderPhotoBlocks(25));
-  galleryOverlay.querySelector('.gallery-overlay-image').src = photoObject[1].url;
-  galleryOverlay.querySelector('.likes-count').textContent = photoObject[1].likes;
-  galleryOverlay.querySelector('.comments-count').textContent = photoObject[1].comments;
+  var photos = createPhotoArr(PHOTOS_COUNT);
+  var photoBlocks = renderPhotoBlocks(photos);
+  picturesContainer.appendChild(photoBlocks);
+  galleryOverlay.querySelector('.gallery-overlay-image').src = photos[0].url;
+  galleryOverlay.querySelector('.likes-count').textContent = photos[0].likes;
+  galleryOverlay.querySelector('.comments-count').textContent = photos[0].comments.length;
   galleryOverlay.classList.remove('hidden');
 };
 
